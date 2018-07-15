7ffcac09b4c439f3f24c69c1e650fdbb69c96f99
farinim <farinim@gmail.com>
2018-07-15 23:37:15 +0530

    python program to check if digits in two numbers are same, second number being the product of first number by 2.

---
+++ identical-digits.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+##Takes input from user: an integer
+##Compares the digits in the user enter integer value and the same integer value multiplied by 2
+##Prints whether the two integer values contain same digits.
+##If no user input provided, the input is defaulted to 142857
+
+import sys
+	
+def getDigits(x):
+	digits = set()
+	for i in range (5,-1,-1):
+		digits.add(x/pow(10,i))
+		x = x%pow(10,i)
+	return digits
+
+def main():
+	if len(sys.argv) == 1:
+		x=142857
+	else:
+		#print sys.argv
+		x = int(sys.argv[1])
+
+	a = getDigits(x)
+	b = getDigits(x*2)
+		
+	#print a,b
+	#print a.symmetric_difference(b)
+	#print a ^ b
+
+	if len(a^b) == 0:
+		print "Same digits in ",x," & ",x*2
+	else:
+		print x," & ", x*2,"do not have same digits"
+	
+
+main()
